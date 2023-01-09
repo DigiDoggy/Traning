6f3add435f1331cf9649346877174e2b24b60026
Ilja Aituganov <Ilja.Aituganov@gmail.com>
2023-01-10 00:41:43 +0200

    recommit

---
+++ src/main/java/com/dmdev/util/CafeConst.java
@@ -0,0 +1,15 @@
+package com.dmdev.util;
+
+public final class CafeConst {
+
+    public static final int BUYER_WAIT_TIME = 5;
+    public static final int MAX_PRODUCT_COUNT = 5;
+    public static final int PRODUCT_TIME_COST = 1;
+    public static final int BUYER_STATS_JOB_PERIOD = 30;
+    public static final int CASHBOX_STATS_JOB_PERIOD = 60;
+
+    private CafeConst() {
+
+    }
+
+}
